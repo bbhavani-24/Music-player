1dbba280e36b1523a5a46f0af6b5249b9cb63310
bbhavani-24 <158707718+bbhavani-24@users.noreply.github.com>
2024-03-02 23:52:56 +0530

    Music-Player

---
+++ app.js
@@ -0,0 +1,32 @@
+let allMusic=[
+    {
+      name:"OMG",
+      artist:"NewJeans",
+      img:"OMG-NJ",
+      src:"OMG-NewJeans"
+    },
+    {
+      name:"Hype Boy",
+      artist:"NewJeans",
+      img:"Hype-Boy-NJ",
+      src:"Music-2"
+    },
+    {
+      name:"Christmas Tree",
+      artist:"V (BTS)",
+      img:"V-CT",
+      src:"Music-3"
+    },
+    {
+      name:"Until I Found You",
+      artist:"Rose (Cover Song)",
+      img:"ROSE",
+      src:"MUSIC-4"
+    },
+    {
+      name:"Suzume",
+      artist:"Toaka",
+      img:"yuta",
+      src:"suzume1"
+    }
+];
